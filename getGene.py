45bb27e30e4524ec2ffec6d845dddb3a0a892be4
Panos Kapetanidis            (CaptainAlready) <pkapetanidis@gmail.com>
2022-07-07 14:22:12 +0300

    Terminal App

--- getGene.py
+++ getGene.py
@@ -2,13 +2,13 @@ import pandas as pd
 import tkinter as tk
 
 
-df = pd.read_csv('GeneTable.csv', sep=';'  , engine='python')
+df = pd.read_csv('GeneTable.csv', sep=';'  , engine='python') #read csv
 
 print("Insert gene Symbol to find possible affymetrix formats")
 
 gene = input()
 
-df = df.loc[df['Gene Symbol'] == gene]
+df = df.loc[df['Gene Symbol'] == gene] #find Affymetrix format
 
-print(df[['ID', 'Sequence Type']]);
+print(df[['ID', 'Sequence Type']]); 
 
